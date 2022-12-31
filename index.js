004d4332a225c9019f56dd853bc1db49b86b5382
harryjsutcliffe <harryjamessutcliffe@gmail.com>
2022-12-31 20:57:05 +0900

    migrate db to mongodb

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -10,40 +11,26 @@ const requestLogger = (request, response, next) => {
 }
 
 const cors = require('cors')
+const Note = require('./models/note')
+
 
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('build'))
 
 
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true
-  }
-]
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
+
 
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
@@ -91,13 +78,24 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
+
+// app.put('/api/notes/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   const note = notes.find(note => note.id != id)
+//   const newNote = {...note, important: response.important}
+
+//   notes.
+
+//   response.status(204).end()
+// })
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
